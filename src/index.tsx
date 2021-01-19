16a5751984c45a0f53fb2d33542b66f0571b25fd
kdichev <konstantin.dichev@gmail.com>
2021-01-19 13:57:29 +0000

    add pulumi

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,9 @@ import * as React from 'react';
 
 // Delete me
 export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+  return (
+    <div>
+      the snozzberries taste like snozzberries env: {process.env.NODE_ENV}
+    </div>
+  );
 };
